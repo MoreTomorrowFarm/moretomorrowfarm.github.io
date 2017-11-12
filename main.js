04c05ede0fd0c608d12639da5c2cfd8a2a360828
Brett <brettatoms@gmail.com>
2017-11-12 09:00:46 -0500

    fix lots of issues with sticky headers, font scaling, navigation, etc

--- main.js
+++ main.js
@@ -1,44 +1,50 @@
 $().ready(function() {
   var titleTop = 0;
-  var initialFontSizeEm = 6;
+
+  // the initialFontSizePx should match px size from .title-overlay in the css
+  // file, e.g. 6em * 16px = 96
+  var initialFontSizePx = 96;
+
+  var $titleOverlayEl = $(".title-overlay");
+  var fontSize = $titleOverlayEl.css("font-size");
+  initialFontSizePx = fontSize.substr(0, fontSize.length - 2);
 
   reset();
   $(window).resize(reset);
   $(window).scroll(updateTitle);
-  $(window).scroll(updateStrips);
+
+  //
+  var elements = document.querySelectorAll("#first-page");
+  Stickyfill.add(elements);
 
   function reset() {
-    const $titleOverlayEl = $(".title-overlay");
+    var $titleOverlayEl = $(".title-overlay");
 
     // recalculate base values we use to calculate everything else
     titleTop = $titleOverlayEl.position().top;
 
-    // var fontSize =
-    //     initialFontSizeEm = parseInt($titleOverlayEl.css("font-size");
-    var fontSize = $titleOverlayEl.css("font-size");
-    console.log(fontSize);
-    // initialFontSizeEm = fontSize.substr(0, fontSize.length - 2);
-    console.log("initialFontSizeEm: ", initialFontSizeEm);
-
     updateTitle();
-    updateStrips();
   }
 
   function updateTitle() {
     var scrollTop = $(window).scrollTop();
     var $el = $(".title-overlay");
+
+    // if we've scrolled passed the top of the title
     if (scrollTop >= titleTop) {
       $el.addClass("fixed-title");
-      const newFontSize = initialFontSizeEm - (scrollTop - titleTop) / 100;
-      if (newFontSize > 2) {
-        $($el).css("font-size", newFontSize + "em");
+
+      // scale the title font size as we scroll
+      var newFontSize = initialFontSizePx - (scrollTop - titleTop) / 10;
+      if (newFontSize > 32) {
+        $($el).css("font-size", newFontSize + "px");
       } else {
-        $($el).css("font-size", "2" + "em");
+        $($el).css("font-size", "32" + "px");
       }
 
       // align items along X-axis if we get beloew a 4em font size
       var $socialIconsEl = $(".social-icons");
-      if (newFontSize < 4) {
+      if (newFontSize < 64) {
         $($socialIconsEl)
           .removeClass("grid-y")
           .addClass("grid-x")
@@ -51,27 +57,16 @@ $().ready(function() {
       }
     } else {
       $el.removeClass("fixed-title");
-      $($el).css("font-size", initialFontSizeEm + "em");
+      $($el).css("font-size", initialFontSizePx + "px");
     }
-  }
-
-  function updateStrips() {
-    var scrollTop = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    var $el = $(".image-strips");
-    if (scrollTop > windowHeight * 0.8) {
-      $el.addClass("fixed-strips");
-    } else {
-      $el.removeClass("fixed-strips");
-    }
-  }
-
-  function scrollTo(id) {
-    $("html, body").animate(
-      {
-        scrollTop: $(id).offset().top
-      },
-      750
-    );
   }
 });
+
+function scrollTo(id) {
+  $("html, body").animate(
+    {
+      scrollTop: $(id).offset().top
+    },
+    750
+  );
+}
